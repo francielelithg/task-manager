9b41f935f5209b7fbb5d091fa3603270fa75f7c8
Franciele Lithg <francielelithg@gmail.com>
2020-11-02 22:22:30 -0300

    add a simple route

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import MainLayout from '../layouts/MainLayout'
 import styles from '../styles/Home.module.css'
 import Typography from '@material-ui/core/Typography'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <MainLayout>
       <div className={styles.container}>
         <Typography variant="h6" gutterBottom>
-          Please, choose if you want to manage tasks or users.
+          Please, choose if you want to manage tasks or <Link href="/users"><a>users</a></Link>.
         </Typography>
       </div>
     </MainLayout>
