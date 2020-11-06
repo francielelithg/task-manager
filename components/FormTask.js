726e788abbedd538d4711b0d498d24ad037ee835
Franciele Lithg <francielelithg@gmail.com>
2020-11-06 00:57:45 -0300

    ui and code adjustments

--- components/FormTask.js
+++ components/FormTask.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import TaskService from '../services/task'
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Button from '@material-ui/core/Button'
 import Checkbox from '@material-ui/core/Checkbox'
 import Dialog from '@material-ui/core/Dialog'
@@ -6,18 +10,18 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import TextField from '@material-ui/core/TextField'
-import TaskService from '../services/task'
-import React from 'react'
 
 const FormTask = (props) => {
   const [dialog, setDialog] = React.useState(true)
-  const [user, setUser] = React.useState(props.user)
+  const [user] = React.useState(props.user)
   const [description, setDescription] = React.useState(props.task ? props.task.description : '')
   const [state, setState] = React.useState(props.task ? props.task.state : false)
+  const [reaload, setReload] = React.useState(false)
 
   const handleSave = (event) => {
     event.preventDefault()
     setDialog(false)
+    setReload(true)
 
     const task = {
       user: user,
@@ -26,67 +30,88 @@ const FormTask = (props) => {
     }
 
     TaskService.createTask(task)
+      .then(() => {
+        props.reload()
+        setReload(false)
+      })
+      .catch(() => {
+        setReload(false)
+      })
     props.passFormUser(false)
   }
 
   const handleUpdate = (event) => {
     event.preventDefault()
     setDialog(false)
+    setReload(true)
 
     props.task.description = description
     props.task.state = state
 
     TaskService.updateTask(props.task)
+      .then(() => {
+        setReload(false)
+      })
+      .catch(() => {
+        setReload(false)
+      })
+
     props.passFormUser(false)
     props.updateSelected(null)
   }
 
   return (
-    <Dialog
-      open={dialog}
-      onClose={(event) => setDialog(false)}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description">
-      <DialogTitle id="alert-dialog-title">{`Add new task`}</DialogTitle>
-      <DialogContent>
-        <TextField
-          disabled
-          id="userName"
-          label="Attribution"
-          value={user.name}
-          type="text"
-          fullWidth
-        />
-        <TextField
-          required
-          id="description"
-          label="Description"
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          type="text"
-          fullWidth
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              id="state"
-              defaultChecked={state}
-              onChange={(event) => setState(event.target.checked)}
-            />
-          }
-          label="Done"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={(event) => {
-          setDialog(false)
-          props.passFormUser(false)
-          props.updateSelected(null)
-        }} color="primary">Cancel</Button>
-        {!props.task && <Button onClick={handleSave} color="secondary" autoFocus>Save</Button>}
-        {props.task && <Button onClick={handleUpdate} color="secondary" autoFocus>Update</Button>}
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog
+        open={dialog}
+        onClose={(event) => setDialog(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{`Add new task`}</DialogTitle>
+        <DialogContent>
+          <TextField
+            disabled
+            id="userName"
+            label="Attribution"
+            value={user.name}
+            type="text"
+            fullWidth
+          />
+          <TextField
+            required
+            id="description"
+            label="Description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            type="text"
+            fullWidth
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                id="state"
+                defaultChecked={state}
+                onChange={(event) => setState(event.target.checked)}
+              />
+            }
+            label="Done"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={(event) => {
+            setDialog(false)
+            props.passFormUser(false)
+            props.updateSelected(null)
+          }} color="primary">Cancel</Button>
+          {!props.task && <Button onClick={handleSave} color="secondary" autoFocus>Save</Button>}
+          {props.task && <Button onClick={handleUpdate} color="secondary" autoFocus>Update</Button>}
+        </DialogActions>
+      </Dialog>
+
+      <Backdrop open={reaload}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
   )
 }
 
--- pages/user/[id].js
+++ pages/user/[id].js
@@ -4,6 +4,8 @@ import TaskService from '../../services/task'
 import { useRouter } from 'next/router'
 import MainLayout from '../../layouts/MainLayout'
 import Button from '@material-ui/core/Button'
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Checkbox from '@material-ui/core/Checkbox'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
@@ -29,6 +31,7 @@ const User = (props) => {
   const [deleteTaskDialog, setDeleteTaskDialog] = React.useState(false)
   const [task, setTask] = React.useState(null)
   const [formTask, setFormTask] = React.useState(false)
+  const [reaload, setReload] = React.useState(false)
 
   const handleBack = () => {
     router.back()
@@ -41,17 +44,36 @@ const User = (props) => {
 
   const handleDelete = (event, user) => {
     setDeleteUserDialog(false)
+    setReload(true)
+
     UserService.deleteUser(user.id)
       .then(() => {
         router.back()
+        setReload(false)
+      })
+      .catch(() => {
+        setReload(false)
       })
   }
 
   const handleDeleteTask = (event) => {
     setDeleteTaskDialog(false)
+    setReload(true)
 
     TaskService.deleteTask(task.id)
+      .then(() => {
+        router.reload()
+        setReload(false)
+      })
+      .catch(() => {
+        setReload(false)
+      })
+
     setTask(null)
+  }
+
+  const reload = () => {
+    router.reload()
   }
 
   return (
@@ -87,10 +109,10 @@ const User = (props) => {
             />
           </ListItemAvatar>
           <ListItemText primary={task.description} />
-          <IconButton edge="end" aria-label="edit-task" onClick={(event) => handleEditTask(event)}>
+          <IconButton size='small' edge="end" aria-label="edit-task" onClick={(event) => handleEditTask(event, task)}>
             <EditIcon />
           </IconButton>
-          <IconButton edge="end" aria-label="delete-task" onClick={(event) => {
+          <IconButton size='small' edge="end" aria-label="delete-task" onClick={(event) => {
             setDeleteTaskDialog(true)
             setTask(task)
             }}>
@@ -101,7 +123,7 @@ const User = (props) => {
 
       {allowEdit && <FormUser user={user} passFormUser={setAllowEdit} />}
 
-      {formTask && <FormTask user={user} task={task} passFormUser={setFormTask} updateSelected={setTask} />}
+      {formTask && <FormTask user={user} task={task} passFormUser={setFormTask} updateSelected={setTask} reload={reload} />}
 
       <Dialog
         open={deleteUserDialog}
@@ -127,6 +149,10 @@ const User = (props) => {
           <Button onClick={(event) => handleDeleteTask(event, user)} color="secondary" autoFocus>Delete</Button>
         </DialogActions>
       </Dialog>
+
+      <Backdrop open={reaload}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </MainLayout>
   )
 }
