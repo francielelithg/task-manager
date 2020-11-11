2b864cdb5b882f639cc7c3a98e875682c176af08
Franciele Lithg <francielelithg@gmail.com>
2020-11-10 23:35:31 -0300

    redirect

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import MainLayout from '../layouts/MainLayout'
-import styles from '../styles/Home.module.css'
-import Typography from '@material-ui/core/Typography'
-import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export default function Home() {
-  return (
-    <MainLayout>
-      <div className={styles.container}>
-        <Typography variant="h6" gutterBottom>
-          Please, choose if you want to manage tasks or <Link href="/users"><a>users</a></Link>.
-        </Typography>
-      </div>
-    </MainLayout>
-  )
+const Home = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/users')
+  })
+
+  return ( null )
 }
+
+export default Home
